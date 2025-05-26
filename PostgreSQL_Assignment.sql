f83b4b070c6a462eaa7a518916bd22a20fe54b3a
Zakaria Islam Robin <zakaria.pau49@gmail.com>
2025-05-26 09:50:48 +0600

    The answers to all the questions

--- PostgreSQL_Assignment.sql
+++ PostgreSQL_Assignment.sql
@@ -13,9 +13,7 @@ INSERT INTO rangers (ranger_id, name, region) VALUES
 (3, 'Felix Hart', 'Savannah Basin'),
 (4, 'Grace Moon', 'Desert Frontier');
 
-
 SELECT * FROM rangers;
-
 
 -- Table - 2 : species
 
@@ -55,5 +53,80 @@ INSERT INTO sightings (sighting_id, species_id, ranger_id, location, sighting_ti
 (3, 3, 4, 'Pangolin Hollow', '2024-06-05 20:30:00', 'Nocturnal activity recorded'),
 (4, 2, 1, 'Bustard Plains', '2024-06-07 08:25:00', 'Solo bird walking across field');
 
-
 SELECT * FROM sightings;
+
+
+-- Question 1 : Register a new ranger with provided data with name = 'Derek Fox' and region = 'Coastal Plains'
+
+INSERT INTO rangers (name, region) VALUES
+('Derek Fox', 'Coastal Plains');
+
+
+-- Question 2 : Count unique species ever sighted.
+
+SELECT COUNT(DISTINCT species_id) AS unique_species_count
+FROM sightings1;
+
+
+-- Question 3 : Find all sightings where the location includes "Pass".
+
+SELECT *
+FROM sightings
+WHERE location LIKE '%Pass%';
+
+
+-- Question 4 : List each ranger's name and their total number of sightings.
+
+SELECT r.name, COUNT(s.sighting_id) AS total_sightings
+FROM rangers r
+LEFT JOIN sightings s ON r.ranger_id = s.ranger_id
+GROUP BY r.name;
+
+
+-- Question 5 : List species that have never been sighted.
+
+SELECT sp.common_name, sp.scientific_name
+FROM species sp
+LEFT JOIN sightings s ON sp.species_id = s.species_id
+WHERE s.sighting_id IS NULL;
+
+
+-- Question 6 : Show the most recent 2 sightings.
+
+SELECT *
+FROM sightings
+ORDER BY sighting_time DESC
+LIMIT 2;
+
+
+-- Question 7 : Update all species discovered before year 1800 to have status 'Historic'.
+
+UPDATE species
+SET conservation_status = 'Historic'
+WHERE discovery_date < '1800-01-01';
+
+
+-- Question 8 : Label each sighting's time of day as 'Morning', 'Afternoon', or 'Evening'.
+
+SELECT 
+  sighting_id,
+  species_id,
+  ranger_id,
+  location,
+  sighting_time,
+  notes,
+  CASE
+    WHEN EXTRACT(HOUR FROM sighting_time) BETWEEN 5 AND 11 THEN 'Morning'
+    WHEN EXTRACT(HOUR FROM sighting_time) BETWEEN 12 AND 17 THEN 'Afternoon'
+    ELSE 'Evening'
+  END AS time_of_day
+FROM sightings;
+
+
+-- Question 9 :  Delete rangers who have never sighted any species
+
+DELETE FROM rangers
+WHERE ranger_id NOT IN (
+  SELECT DISTINCT ranger_id
+  FROM sightings
+);
